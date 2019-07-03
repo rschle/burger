0a38cde74b25f178751f11def2477e1faaca7fc7
Rebecca Schleimer <rschle@sas.upenn.edu>
2019-07-03 10:06:15 -0700

    started working on burger_controller.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,15 +5,23 @@ const router = express.Router();
 // Import the model (burger.js) to use its database functions.
 const burger = require("../models/burger.js");
 
+
+//now we have to create our routes (using elements from orm.js as well as cat.js)
 router.get("/", (req, res) => {
-    burger.all(data => {
-      const hbsObject = {
-        burgers: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
-    });
+  burger.selectAll(data => {
+    const hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
   });
+});
+
+router.post("/api/burgers", (req, res) => {
+  burger.insertOne(["burger_name"], [req.body.name], result => {
+    res.json({id: result.insertId});
+  });
+});
 
 
 
