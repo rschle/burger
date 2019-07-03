6f9edca9c4611faa68bbc4fef16b377f29712308
Rebecca Schleimer <rschle@sas.upenn.edu>
2019-07-03 09:31:39 -0700

    updared burger.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,9 +2,18 @@ const express = require("express");
 
 const router = express.Router();
 
+// Import the model (burger.js) to use its database functions.
 const burger = require("../models/burger.js");
 
-
+router.get("/", (req, res) => {
+    burger.all(data => {
+      const hbsObject = {
+        burgers: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
 
 
 
--- models/burger.js
+++ models/burger.js
@@ -1,9 +1,22 @@
 const orm = require("../config/orm.js")
 
-// const burger = {
-    
-//     orm.selectAll("burgers", res)
-// }
+const burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", res => {
+            cb(res);
+        });
+    },
+    insertOne: function(cols, vals, cb){
+        orm.insertOne("burgers", cols, vals, res => {
+            cb(res);
+        });
+    },
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, res => {
+            cb(res);
+        });
+    }
+};
 
 
 module.exports = burger;
