b0a9880793bfa5b0c641eb601b1b4f8a8f3977cf
Rebecca Schleimer <rschle@sas.upenn.edu>
2019-07-03 14:53:33 -0700

    started working on handlebars layout

--- db/schema.sql
+++ db/schema.sql
@@ -4,6 +4,8 @@ CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(255) NOT NULL,
-    devoured BOOLEAN DEFAULT false
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
+
+SELECT * FROM burgers
--- server.js
+++ server.js
@@ -1,1 +1,24 @@
 var express = require("express");
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_Controller.js");
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log("App now listening at localhost:" + PORT);
+});
